e427ef45762115e01ced79a23cb01edb7a32aa00
GYAN DASH <gyanaranjan.dash5@gmail.com>
2021-01-16 21:51:52 +0530

    Add project files.

---
+++ MVC_CRUD_DBFirst/Controllers/CustomerController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_CRUD_DBFirst.Models;
+
+namespace MVC_CRUD_DBFirst.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (DbModel dbModel = new DbModel())
+            {
+                return View(dbModel.Customers.ToList());
+            }
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DbModel dbModel = new DbModel())
+            {
+                return View(dbModel.Customers.Where(x => x.Id ==id).FirstOrDefault());
+            }
+            
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            try
+            {
+                using (DbModel dbModel = new DbModel())
+                {
+                    dbModel.Customers.Add(customer);
+                    dbModel.SaveChanges();
+
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DbModel dbModel = new DbModel())
+            {
+                return View(dbModel.Customers.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            try
+            {
+                using (DbModel dbModel =new DbModel())
+                {
+                    dbModel.Entry(customer).State = EntityState.Modified;
+                    dbModel.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DbModel dbModel = new DbModel())
+            {
+                return View(dbModel.Customers.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (DbModel dbModel = new DbModel())
+                {
+                    Customer customer = dbModel.Customers.Where(x => x.Id == id).FirstOrDefault();
+                    dbModel.Customers.Remove(customer);
+                    dbModel.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVC_CRUD_DBFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_CRUD_DBFirst.MvcApplication" Language="C#" %>
